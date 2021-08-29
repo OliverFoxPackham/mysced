bf15ca177852f48ceecd5628057eeb518b462d2c
OliverFoxPackham <oliverfoxpackham@gmail.com>
2021-08-28 23:13:51 -0700

    Initial push

---
+++ code/days.js
@@ -0,0 +1,12 @@
+const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+var text = "";
+
+function startUp() {
+    var d = new Date();
+    var n = d.getDay();
+    for (var i = n; i-n < 7; i++) {
+    text += '<div class="daybox"><div class="dayname">' + days[i%7] + " 7:00" + '</div></div>';
+    }
+console.log(text);
+document.getElementById("daysAndNav").innerHTML = text;
+}
