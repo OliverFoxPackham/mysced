5d208a3b82f5dd1c266e3c9b7c9dd928c1841f69
OliverFoxPackham <oliverfoxpackham@gmail.com>
2021-09-03 09:09:23 -0700

    moving index.html to root

--- code/days.js
+++ code/days.js
@@ -5,7 +5,7 @@ function startUp() {
     var d = new Date();
     var n = d.getDay();
     for (var i = n; i-n < 7; i++) {
-    text += '<div class="daybox"><div class="dayname">' + days[i%7] + " 7:00" + '</div></div>';
+    text += '<div class="daybox"><div class="dayname">' + days[i%7] + " 7:00" + '</div><div class="schedule"></div></div>';
     }
 console.log(text);
 document.getElementById("daysAndNav").innerHTML = text;
---
+++ code/events.js
@@ -0,0 +1,6 @@
+var event = [wkday,date,startt,endt,color,activity]
+var events
+
+document.getElementById("p2").style.color = "blue";
+
+document.getElementsByClassName("schedule").innerHTML = text;
